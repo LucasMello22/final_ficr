a55c19c115171c8dc97ed2955bde785808d13c33
Lucas Mello22 <lucas.s.mello@outlook.com>
2024-07-04 07:59:55 -0300

    Stopped at video 5

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 // ignore_for_file: unused_import
-
+import 'package:final_ficr/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:final_ficr/login.dart';
 import 'package:final_ficr/interna.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MaterialApp(
     debugShowCheckedModeBanner: false,
     home: Interna(),
